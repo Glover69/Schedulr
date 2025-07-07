9a058b7c84493ac6fe76a27beb023702757b58bc
Glover69 <iamglover02@gmail.com>
2025-07-07 17:26:01 +0000

    started dashboard view

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideLottieOptions } from 'ngx-lottie';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+          }),
+    provideLottieOptions({
+      player: () => import('lottie-web'),
+    }),
+
+  ]
 };
---
+++ src/models/data.models.ts
@@ -0,0 +1,28 @@
+export type Schedule = {
+  semester: semester;
+  classes: Class[];
+}
+
+export type Class = {
+  course_name: string; // e.g., "Intro to AI"
+  days: Days[]; // e.g., ["Monday", "Wednesday"]
+  color: string;
+  color_light: string; // e.g., "#B54708", "#FFFAEB"
+}
+
+export type semester = {
+  id: string; // Unique identifier for the semester, e.g., "fall-2025"
+  schedule_name: string; // e.g., "Fall 2025"
+  start_date: string; // e.g., "2025-08-05"
+  end_date: string; // e.g., "2025-12-15"
+  excluded_dates?: string[]; // e.g., ["2025-10-10", "2025-11-01"]
+};
+
+export type Days = {
+  day: string; // e.g., "Monday"
+  start_time: any
+  end_time: any
+  room: string
+}
+
+
