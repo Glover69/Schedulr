7e50a02dd4609a580224167f153cd44079fba6af
Glover69 <iamglover02@gmail.com>
2025-08-18 20:58:04 +0000

    preparing proxy for prod

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode, APP_INITIALIZER } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+  APP_INITIALIZER,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,18 +13,21 @@ import { GoogleAuthService } from '../services/google-auth.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 if (!isDevMode()) {
-    // Disable all console logging in production
-    console.log = () => {};
-    console.info = () => {};
-    console.warn = () => {};
-    console.error = () => {};
+  // Disable all console logging in production
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
     provideLottieOptions({
       player: () => import('lottie-web'),
     }),
@@ -30,5 +38,5 @@ export const appConfig: ApplicationConfig = {
       useFactory: (auth: GoogleAuthService) => () => auth.hydrate(),
       deps: [GoogleAuthService],
     },
-  ]
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 // environment.ts
 export const environment = {
-  // apiBase: "http://localhost:2005",
   apiBase: "https://centralbackend-zkz2.onrender.com",
   production: false,
   googleClientId: '601892200759-q9ktt6gjis0a9d5h3mthj48fuh4422se.apps.googleusercontent.com',
